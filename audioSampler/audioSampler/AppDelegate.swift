4f3750b0bfb6ccb0ede027d5575ea2f6f44c7b2e
Qiao Lin <qiaoclin@gmail.com>
2017-03-23 11:29:59 +0100

    add Pod for frameworks

--- audioSampler/audioSampler/AppDelegate.swift
+++ audioSampler/audioSampler/AppDelegate.swift
@@ -9,6 +9,8 @@
 import UIKit
 import CoreData
 import AVFoundation
+import Firebase
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,7 +21,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-
+        // configure
+        FIRApp.configure()
+        
+        // audio settings
         try? AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryAmbient, with: .interruptSpokenAudioAndMixWithOthers)// app plays while another app is also playing except for speech
         
         return true
